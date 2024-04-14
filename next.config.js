3dbd7f2e6b49c509f2c2ba1a05d6ca582cf69589
smj0503 <wovj0503@gmail.com>
2024-04-14 19:43:12 +0900

    [Bug Fix : Sony]
    Antd import 관련 에러 수정 중

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 
 const nextTranslate = require('next-translate-plugin');
 const Dotenv = require('dotenv-webpack');
+const withTM = require('next-transpile-modules')([
+  'antd',
+  '@ant-design/icons',
+])
 
-module.exports = nextTranslate({
+module.exports = withTM(nextTranslate({
   webpack: (config) => {
     // 기존의 웹팩 플러그인에 새로운 Dotenv플러그인을 연결시켜준다.
     // silent는 옵션은 .env파일을 찾지 못했을 때 에러를 일으키지 않도록 설정해주는 옵션이다.
@@ -15,4 +19,4 @@ module.exports = nextTranslate({
 
     return config;
   },
-});
+}));
