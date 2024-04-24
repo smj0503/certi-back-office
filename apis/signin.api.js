a38da5b2690c92ced18536ccf04df55c5ab025c7
smj0503 <wovj0503@gmail.com>
2024-04-24 22:29:00 +0900

    로그인 API 수정

--- apis/signin.api.js
+++ apis/signin.api.js
@@ -3,6 +3,6 @@ import { api } from '@/apis/index';
 export const signIn = async (username, password) => {
   return await api.post('/auth/backoffice/login', {
     username: username,
-    password: password
+    password: password,
   });
 };
